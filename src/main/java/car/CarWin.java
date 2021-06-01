babfa6b76a99611707fbe7bd31d9b3b341e64340
Junpin <tyuighjk86@gmail.com>
2021-06-01 15:06:11 +0800

    JFrame視窗 WIDTH常數名稱全大寫表示之後不會改他

---
+++ src/main/java/car/CarWin.java
@@ -0,0 +1,13 @@
+package car;
+
+import javax.swing.*;
+
+public class CarWin {
+    static final int WIDTH = 800;
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setSize(WIDTH,600);
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+}
